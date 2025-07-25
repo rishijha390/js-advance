53fa232f1b369849e0bfbf6f129d4c86c00942ad
rishijha390 <rishijha390@gmail.com>
2025-07-26 00:25:18 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -156,15 +156,27 @@
 //    device.textContent = `${dets.target.value} Device Selected`;
 // })
 
-let h1 = document.querySelector("h1");
-window.addEventListener("keydown" , function (dets) {
-   if(dets.key === " "){
-      h1.textContent = "SPC";
-   }
-else {
-   h1.textContent = dets.key;
-}
+// let h1 = document.querySelector("h1");
+// window.addEventListener("keydown" , function (dets) {
+//    if(dets.key === " "){
+//       h1.textContent = "SPC";
+//    }
+// else {
+//    h1.textContent = dets.key;
+// }
+// });
+
+let btn = document.querySelector("#btn");
+let fileinp = document.querySelector("#fileinp");
+ btn.addEventListener("click" , function(){
+    fileinp.click();
+ });
+
+ fileinp.addEventListener("change" , function(dets){
+   // console.log(dets.target.files[0].name);
+  // btn.textContent= dets.target?.files[0].name;
+  const file = dets.target.files[0];
+  if(file){
+    btn.textContent = file.name;
+  }
 });
-
-
-
