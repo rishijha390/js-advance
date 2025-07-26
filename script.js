be9230d65a62cbfa6571febfb70142c9ec7f03be
rishijha390 <rishijha390@gmail.com>
2025-07-27 00:36:25 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -166,17 +166,57 @@
 // }
 // });
 
-let btn = document.querySelector("#btn");
-let fileinp = document.querySelector("#fileinp");
- btn.addEventListener("click" , function(){
-    fileinp.click();
- });
+// let btn = document.querySelector("#btn");
+// let fileinp = document.querySelector("#fileinp");
+//  btn.addEventListener("click" , function(){
+//     fileinp.click();
+//  });
 
- fileinp.addEventListener("change" , function(dets){
-   // console.log(dets.target.files[0].name);
-  // btn.textContent= dets.target?.files[0].name;
-  const file = dets.target.files[0];
-  if(file){
-    btn.textContent = file.name;
-  }
-});
+//  fileinp.addEventListener("change" , function(dets){
+//    // console.log(dets.target.files[0].name);
+//   // btn.textContent= dets.target?.files[0].name;
+//   const file = dets.target.files[0];
+//   if(file){
+//     btn.textContent = file.name;
+//   }
+// });
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();
+    // console.log(
+    //     inputs[0].value,
+    //     inputs[1].value,
+    //     inputs[2].value,
+    //     inputs[3].value
+    // );
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
+
+    let img = document.createElement("img");
+    img.setAttribute("src",inputs[0].value);
+    
+    let h3 = document.createElement("h3");
+    h3.textContent = inputs[1].value;
+    let h5 = document.createElement("h5");
+    h5.textContent = inputs[2].value;
+    let p = document.createElement("p");
+    p.textContent = inputs[3].value;
+
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+
+    main.appendChild(card);
+    inputs.forEach(function(inp){
+        if(inp.type !== "submit"){
+             inp.value = "";
+        } 
+    }); 
+}); 
