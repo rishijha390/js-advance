0a5b2a2fcdc59538da9328c59bb2385d43e837c0
rishijha390 <rishijha390@gmail.com>
2025-07-27 22:42:07 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -181,42 +181,56 @@
 //   }
 // });
 
-let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input");
-let main = document.querySelector("#main");
-form.addEventListener("submit",function(dets){
-    dets.preventDefault();
-    // console.log(
-    //     inputs[0].value,
-    //     inputs[1].value,
-    //     inputs[2].value,
-    //     inputs[3].value
-    // );
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let profile = document.createElement("div");
-    profile.classList.add("profile");
+// let form = document.querySelector("form");
+// let inputs = document.querySelectorAll("input");
+// let main = document.querySelector("#main");
+// form.addEventListener("submit",function(dets){
+//     dets.preventDefault();
+//     // console.log(
+//     //     inputs[0].value,
+//     //     inputs[1].value,
+//     //     inputs[2].value,
+//     //     inputs[3].value
+//     // );
+//     let card = document.createElement("div");
+//     card.classList.add("card");
+//     let profile = document.createElement("div");
+//     profile.classList.add("profile");
 
-    let img = document.createElement("img");
-    img.setAttribute("src",inputs[0].value);
+//     let img = document.createElement("img");
+//     img.setAttribute("src",inputs[0].value);
     
-    let h3 = document.createElement("h3");
-    h3.textContent = inputs[1].value;
-    let h5 = document.createElement("h5");
-    h5.textContent = inputs[2].value;
-    let p = document.createElement("p");
-    p.textContent = inputs[3].value;
+//     let h3 = document.createElement("h3");
+//     h3.textContent = inputs[1].value;
+//     let h5 = document.createElement("h5");
+//     h5.textContent = inputs[2].value;
+//     let p = document.createElement("p");
+//     p.textContent = inputs[3].value;
 
-    profile.appendChild(img);
-    card.appendChild(profile);
-    card.appendChild(h3);
-    card.appendChild(h5);
-    card.appendChild(p);
+//     profile.appendChild(img);
+//     card.appendChild(profile);
+//     card.appendChild(h3);
+//     card.appendChild(h5);
+//     card.appendChild(p);
 
-    main.appendChild(card);
-    inputs.forEach(function(inp){
-        if(inp.type !== "submit"){
-             inp.value = "";
-        } 
-    }); 
-}); 
+//     main.appendChild(card);
+//     inputs.forEach(function(inp){
+//         if(inp.type !== "submit"){
+//              inp.value = "";
+//         } 
+//     }); 
+// }); 
+// let abcd = document.querySelector("#abcd");
+// abcd.addEventListener("mouseover" , function() {
+//     abcd.style.backgroundColor ="yellow";
+// });
+
+// abcd.addEventListener("mouseout" , function() {
+//     abcd.style.backgroundColor ="red";
+// });
+
+let abcd = document.querySelector("#abcd");
+window.addEventListener("mousemove" , function(dets){
+    abcd.style.top = dets.clientY + "px";
+    abcd.style.left = dets.clientX + "px";
+});
