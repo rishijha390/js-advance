f93632199ebcdb462f5ae9a08b662d9b18060b1b
rishijha390 <rishijha390@gmail.com>
2025-07-27 23:57:07 +0530

    Updated the learnings

--- script.js
+++ script.js
@@ -242,9 +242,27 @@
 // let nav = document.querySelector("#nav").addEventListener("click",function(){
 //     alert("clicked");
 // })
-let ul=document.querySelector("ul");
-ul.addEventListener("click",function(dets){
-   // dets.target.style.textDecoration = "line-through";
-   //dets.target.classList.add("lt");
-    dets.target.classList.toggle("lt");
+// let ul=document.querySelector("ul");
+// ul.addEventListener("click",function(dets){
+//    // dets.target.style.textDecoration = "line-through";
+//    //dets.target.classList.add("lt");
+//     dets.target.classList.toggle("lt");
+// });
+
+let a = document.querySelector(".a");
+let b = document.querySelector(".b");
+let c = document.querySelector(".c");
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" , function(){
+    console.log("button clicked");
 });
+c.addEventListener("click" , function(){
+    console.log("c clicked");
+}, true);
+b.addEventListener("click" , function(){
+    console.log("b clicked");
+});
+a.addEventListener("click" , function(){
+    console.log("a clicked");
+},true);
